ca014b9e2799bc560e493656547d1d5495e14b08
Linus <36166951+LinusMadera@users.noreply.github.com>
2023-06-10 15:57:34 -0300

    Main components and assets added

---
+++ front-capstone/src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+const Header = () => {
+    return(
+        <header>
+            <img src="./logo.svg" alt="restaurant logo"/>
+            <Nav/>
+        </header>
+    )
+}
+
+export default Header;
+
---
+++ front-capstone/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <ul>
+        <li>
+            <a href='/Home'>Home</a>
+            <a href='/About'>About</a>
+            <a href='/Menu'>Menu</a>
+            <a href='/Reservations'>Reservations</a>
+            <a href='/Order Online'>Order Online</a>
+            <a href='/Login'>Login</a>
+        </li>
+    </ul>
+  )
+}
